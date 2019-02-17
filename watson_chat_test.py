34a994a1edca7a8af43125786862cb4bf4a03e82
John Kim <johnary_kim@hotmail.com>
2019-02-17 11:49:23 -0500

    Chat add

--- watson_chat_test.py
+++ watson_chat_test.py
@@ -7,26 +7,29 @@ service = AssistantV2(
     url='https://gateway-wdc.watsonplatform.net/assistant/api'
 )
 
-
 sessionID = service.create_session(
     assistant_id='d3bdd0a5-4f23-490b-98d4-1e258befb228'
 ).get_result()
-print(sessionID["session_id"])
 
-''' 
-python3 watson_chat_test.py prints above
-{
-  "session_id": "7bc1ef4b-3bb5-4c90-91b2-9dc90debe4c4"
-}
-'''
+print("Hello. Welcome to SecAware! What can I help you with? I can help you learn about various IT concepts.")
 
-response = service.message(
-    assistant_id='d3bdd0a5-4f23-490b-98d4-1e258befb228',
-    session_id= sessionID["session_id"],
-    input={
-        'message_type': 'text',
-        'text': 'I want to know more about malware'
-    }
-).get_result()
+user_text = input("Respond, then press ENTER.")
 
-print(json.dumps(response, indent=2))
+while(user_text != "exit" or user_text != "quit"):
+
+    response = service.message(
+        assistant_id='d3bdd0a5-4f23-490b-98d4-1e258befb228',
+        session_id= sessionID["session_id"],
+        input={
+            'message_type': 'text',
+            'text': user_text
+        }
+    ).get_result()
+
+    text_response = json.dumps(response)
+    text_response = response["output"]["generic"][0]["text"]
+    print(text_response) 
+
+    user_text = input("Respond, then press ENTER.")
+
+ 
